adb320adb2f0a86d3d6ac141c3c6f034a01392f1
Lesliee Cruz <lesliee.icc@gmail.com>
2022-07-15 14:19:12 -0600

    week 8

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -20,7 +20,8 @@ export class ContactListComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
 
-    this.contacts = this.contactService.getContacts()
+    this.contacts = this.contactService.getContacts();
+
     this.subscription = this.contactService.contactListChangedEvent
       .subscribe((contacts: Contact[]) => {
         this.contacts = contacts;
